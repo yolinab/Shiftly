7f211914dfe6a3d1a59077b0268a971bac36c45e
Yolina Yordanova <y.yordanova@student.utwente.nl>
2025-02-02 21:51:29 +0100

    Initial project setup

---
+++ backend/index.ts
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { Pool } = require("pg");
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Database Connection
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+});
+
+// Test Route
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
